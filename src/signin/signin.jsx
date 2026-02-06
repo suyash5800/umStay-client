c38b343a58a5f7c1f92de36d26abed4d8203a417
Suyash Ranchandra Patil <suyapatil5800@gmail.com>
2026-02-06 15:12:09 +0530

    updated a sign component

---
+++ src/signin/signin.jsx
@@ -0,0 +1,20 @@
+const Signin=()=>{
+
+    return(
+    <div className="Container-fluid justify-content-center align-items-center d-flex  vh-100"> 
+     <div className="row w-100">
+        <div className="col-6 bg-primary text-white h-100 d-flex flex-column justify-content-center align-items-center ">
+            <h1 className="text-center">Welcome Back</h1>
+            <p className="text-center">To keep connected with us please login with your personal info</p>
+            
+        </div>
+        <div className="col-6 bg-danger text-white">
+            <h1>login form </h1>
+        </div>
+     </div>
+       
+
+    </div>
+    );
+}
+export default Signin;
