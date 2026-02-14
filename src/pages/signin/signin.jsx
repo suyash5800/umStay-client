3274de28bcc91c884844532a360a0695fd592def
Suyash Ranchandra Patil <suyapatil5800@gmail.com>
2026-02-14 19:44:39 +0530

    trying dash

--- src/pages/signin/signin.jsx
+++ src/pages/signin/signin.jsx
@@ -9,11 +9,11 @@ const Signin = () => {
 
                 <div className="col-lg-6 d-none d-lg-flex justify-content-center align-items-center bg-primary text-white">
                     <div className="text-center px-4">
-                          <h1 className="fw-bold">Welcome Back</h1>
-                    <p className="lead">To keep connected with us please login with your personal info</p>
+                        <h1 className="fw-bold">Welcome Back</h1>
+                        <p className="lead">To keep connected with us please login with your personal info</p>
 
                     </div>
-                  
+
                 </div>
                 <div className="col-lg-6 col-md-8 col-sm-10 mx-auto  border-1 text-black bg-rightside  ">
                     <div className="card shadow-lg border-0 p-4">
@@ -34,18 +34,18 @@ const Signin = () => {
                                 <div className="d-flex justify-content-between align-items-center mb-3">
                                     <div>
                                         <input type="checkbox" className="form-check-input" />
-                                        <label  className="form-check-label">Remember me</label>
+                                        <label className="form-check-label">Remember me</label>
 
                                     </div>
                                     <a href="#" className="text-decoration-none">Fogot passward</a>
                                 </div>
                                 {/*Login button*/}
-                               <button className="btn btn-primary w-100">login</button>
+                                <button className="btn btn-primary w-100">login</button>
 
                             </form>
                             {/*Register*/}
                             <p className="text-center"> Dont have a account? <Link to="/signup" className="text-decoration-none">Sign up</Link></p>
-
+                            <Link to="/dashboard" className="text-decoration-none">Go to Dashboard</Link>
 
                         </card>
                     </div>
@@ -53,7 +53,7 @@ const Signin = () => {
                 </div>
             </div>
 
-        <Link to="/dashboard" className="text-decoration-none">Go to Dashboard</Link>
+
         </div>
     );
 }
