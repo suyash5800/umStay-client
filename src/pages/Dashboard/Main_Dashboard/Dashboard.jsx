f85ad6015ccc8b0363538d892a5cb696336605bd
Suyash Ranchandra Patil <suyapatil5800@gmail.com>
2026-03-01 01:41:24 +0530

    try: debug dash

--- src/pages/Dashboard/Main_Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Main_Dashboard/Dashboard.jsx
@@ -41,12 +41,12 @@ const Dashboard = () => {
     //     verifyUser();
     // }, [navigate]);
 
-    if (loading) {
-        return (
-            <div className="d-flex justify-content-center align-items-center vh-100 bg-light">
-                <div className="spinner-border text-primary" role="status"></div>
-            </div>
-        );
+    // if (loading) {
+    //     return (
+    //         <div className="d-flex justify-content-center align-items-center vh-100 bg-light">
+    //             <div className="spinner-border text-primary" role="status"></div>
+    //         </div>
+    //     );
     }
 
     return (
